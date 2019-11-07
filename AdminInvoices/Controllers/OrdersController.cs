2fd00a10a0b2f4432b08c7f3adada0363a6c0805
SHERRIS <T7039352@live.tees.ac.uk>
2019-11-07 17:02:09 +0000

    Add project files.

---
+++ AdminInvoices/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminInvoices.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            //needs to get all orders <- is that needed, usefull or practical ????
+
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> Pending()
+        {
+            //needs toget orders where invoiced= true AND disptched= false
+
+           //where order.invoiced = rue && where order.Dispatched = False
+
+            return View(await .ToListAsync());
+        }
+
+
+
+        }
+}
---
+++ AdminInvoices/TempData/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdminInvoices
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Invoiced { get; set; }
+        public bool Dispatched { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
